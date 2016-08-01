8db28f53d40ba73618481998efbd0e63bf824c5c
Administrator <316024763@qq.com>
2016-08-01 18:06:07 +0800

    // left移动完成，有bug

--- main2048.js
+++ main2048.js
@@ -6,12 +6,13 @@ var borad = [];
 
 //创建新游戏
 function newGarm() {
-    init();    
+    init();
+    generateOneNumber();
+    generateOneNumber();
 }
 
 //游戏初始化
 function init() {
-
 
     for( var i = 0; i < 4; i++ ){
 
@@ -26,9 +27,149 @@ function init() {
         }
     }
 
+    for( let i = 0; i < 4; i++ ){
+        borad[i] = [];
+        for( let j =0; j < 4; j++ ){
+            borad[i][j] = 0;
+        }
+    }
+
+    updateBoardView();
+}
+
+
+function updateBoardView(){
+    $(".gird-cell-num").remove();
+
+    for( let i =0; i < 4; i++ ){
+        for( let j =0; j < 4; j++ ){
+            var girdCellNum = $("<div class='gird-cell-num' id='gird-cell-num-"+i+"-"+j+"'></div>")
+
+            if( borad[i][j] == 0 ){
+                girdCellNum.css({
+                    width: 0,
+                    height: 0,
+                    top: getTop(i,j)+50,
+                    left: getLeft(i,j)+50
+                });
+            }else{
+                girdCellNum.css({
+                    width: 100+"px",
+                    height: 100 + "px",
+                    top: goTop(i,j),
+                    left: goTop(i,j),
+                    backgroundColor: getNumberBackgroundColor(borad[i][j])
+                });
+            }
+
+
+            $("#grid-container").append(girdCellNum);
+        }
+    }
 
 }
 
+function generateOneNumber(){
+    if( nospace( borad ) ){
+        return false;
+    }
+
+    var randX = parseInt(Math.floor(Math.random()*4));
+    var randY = parseInt(Math.floor(Math.random()*4));
+
+    var tempNumber = 0;
+
+    while( tempNumber < 50 ){
+        if( borad[randX][randY] == 0 ){
+            break;
+        }
+        randX = parseInt(Math.floor(Math.random()*4));
+        randY = parseInt(Math.floor(Math.random()*4));
+
+        tempNumber++;
+    }
+
+    if( tempNumber == 50 ){
+        for( let i =0; i < 4; i++ ){
+            for( let j =0; j < 4; j++ ){
+                if( borad[i][j] == 0 ){
+                    randX = i;
+                    randY = j;
+                }
+            }
+        }
+    }
+
+    var randNumber = Math.random() < 0.25 ? 2 : 4;
+
+    borad[randX][randY] = randNumber;
+    showNumberWithAnimation( randX, randY, randNumber);
+
+    return true;
+}
+function canMoveLeft( borad ){
+    for( let i =0; i < 4; i++ ){
+        for( let j =0; j < 4; j++ ){
+            if(borad[i][j] != 0){
+                if( borad[i][j-1] == 0 || borad[i][j] == borad[i][j-1]){
+                    return 0;
+                }
+            }
+        }
+    }
+}
+function MoveLeft(){
+    if( canMoveLeft(borad)){
+        return;
+    }
+
+    for( let i =0; i < 4; i++ ){
+        for( let j =0; j < 4; j++ ){
+
+            if( borad[i][j] != 0 ){
+                for( let k = 0; k < j; k++ ){
+                    console.log(borad[i][k]==0 && noBlockHorizontal(i,k,j,borad))
+                    if( borad[i][k]==0 && noBlockHorizontal(i,k,j,borad)){
+                        showMoveAnimation(i,j,i,k);
+                        borad[i][k] = borad[i][j];
+                        borad[i][j] = 0;
+                        continue;
+                    }else{
+                        continue;
+                    }
+                }
+            }
+        }
+    }
+
+}
+
+
 $(document).ready(function () {
    newGarm();
+    $(document).on("keydown", function(event){
+
+        switch (event.keyCode){
+            case 37:
+                if( MoveLeft() ){
+
+                }
+                break;
+            case 38:
+                if( MoveTop() ){
+
+                }
+                break;
+            case 39:
+                if( MoveRight() ){
+
+                }
+                break;
+            case 40:
+                if( MoveBottom() ){
+
+                }
+                break;
+        }
+    });
 });
--- tools.js
+++ tools.js
@@ -9,3 +9,77 @@ function getTop( x, y) {
 function getLeft( x, y) {
     return 10 + y * 110 ;
 }
+
+function randDomNum(){
+    return parseInt(Math.floor(Math.random()*4));
+}
+
+function getNumberBackgroundColor(number){
+    switch (number){
+        case 2:return "#eee4da";break;
+        case 4:return "#ede0c8";break;
+        case 8:return "#f2b179";break;
+        case 16:return "#f59563";break;
+        case 32:return "#f67c5f";break;
+        case 64:return "#f65e3b";break;
+        case 128:return "#edcf72";break;
+        case 256:return "#edcc61";break;
+        case 512:return "#9c0";break;
+        case 1024:return "#33b5e5";break;
+        case 2048:return "#09c";break;
+        case 4096:return "#a6c";break;
+        case 8192:return "#93c";break;
+    }
+    return "black";
+}
+
+function getNumberColor( number ){
+    if( number <= 4 )
+        return "#776e65";
+
+    return "white";
+}
+
+function nospace( board ){
+    for( let i = 0; i < 4; i++ ){
+        for( let j =0; j < 4; j++ ){
+            if( borad[i][j] === 0) {
+                return false;
+            }
+        }
+    }
+    return false;
+}
+
+function noBlockHorizontal(row,k,j,board){
+    for( let i = k+1; i < j; i++ ){
+        if(board[row][i] != 0){
+            return false;
+        }
+    }
+    return true;
+}
+
+function showNumberWithAnimation( i, j, n ){
+
+    var numberCell = $("#gird-cell-num-"+i+"-"+j);
+    numberCell.css({
+        backgroundColor: getNumberBackgroundColor( n ),
+        color: getNumberColor(n)
+    });
+    numberCell.text( n );
+    numberCell.animate({
+        width: "100px",
+        height: "100px",
+        top: getTop(i,j),
+        left: getLeft(i,j)
+    },50);
+}
+
+function showMoveAnimation(row,i,row,j){
+    var numberCell = $("#gird-cell-num-"+row+"-"+i);
+    numberCell.animate({
+        top: getTop(row,j),
+        left: getLeft(row,j)
+    },200);
+}
